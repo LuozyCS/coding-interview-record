f5c6bd3347ab9d37ea349a04f2b4f9bfc16f23f4
Luozy_CS <Luozy_CS@163.com>
2023-12-05 23:38:30 +0800

    790

--- Acwing/basic/790.求立方根_二分or牛顿迭代.py
+++ Acwing/basic/790.求立方根_二分or牛顿迭代.py
@@ -1,3 +1,4 @@
+# 普通二分 直接找
 n = float(input())
 def bsearch(l, r):
     while r - l > 1e-8: #浮点数二分法的循环条件是r-l<1e-8,其中8是因为题目中要保留6位小数
@@ -10,3 +11,5 @@ def bsearch(l, r):
 
 l = bsearch(-10000, 10000)
 print("{:.6f}".format(l))
+
+# 其实也可以用牛顿迭代法。
