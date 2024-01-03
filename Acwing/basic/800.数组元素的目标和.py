674ab2c86648ee40486436692f354c47ea12a5ee
Luozy_CS <Luozy_CS@163.com>
2024-01-03 19:56:35 +0800

    800 思路和799一样

---
+++ Acwing/basic/800.数组元素的目标和.py
@@ -0,0 +1,20 @@
+# 仍然是双指针的做法，参考799
+
+n, m, x = map(int, input().split())
+N = list(map(int, input().split()))
+M = list(map(int, input().split()))
+# 防list out of range bug
+
+def main():
+    j = 0
+    for i in range(n):
+        while (N[i] + M[j]) > x:
+            j -= 1
+        while j < m - 1 and (N[i] + M[j]) < x:
+            j += 1
+        if (N[i] + M[j]) == x:
+            print (i, j)
+            break
+    return
+
+main()
