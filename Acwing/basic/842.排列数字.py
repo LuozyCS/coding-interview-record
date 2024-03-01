fd0eee857870ba92d3c1f7719e19bcc9f369f82f
Luozy_CS <Luozy_CS@163.com>
2024-03-01 15:40:48 +0800

    dfs就是递归

--- Acwing/basic/842.排列数字.py
+++ Acwing/basic/842.排列数字.py
@@ -7,16 +7,19 @@ n = 0
 def dfs(x):
     global path, state, n
     if x == n:
-        print(path[:n])
+        for i in range(n):
+            print (path[i] , end = " ")
+        print()
         return
     for i in range(n):
         if state[i] == 0:
-            path[x] = i
+            path[x] = i + 1
             state[i] = 1
             dfs(x + 1)
             state[i] = 0
 
 def  main():
+    global n
     n = int(input())
     dfs(0)
 main()
