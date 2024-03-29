422fdc9e3dc8e0ab4a1dad9f6093b999302c8a40
Luozy_CS <Luozy_CS@163.com>
2024-03-29 20:07:21 +0800

    sql where < 3

--- MySQL/185.部门工资前三的所有员工.sql
+++ MySQL/185.部门工资前三的所有员工.sql
@@ -1,1 +1,15 @@
--- 不是前1, 不一样的, 需要分组
+-- 不是前1, 需要distinct
+# Write your MySQL query statement below
+select 
+    D.name as Department, 
+    E.name as Employee, 
+    E.salary as Salary
+from 
+    Employee E
+join 
+    Department D on D.id = E.departmentId
+where (
+    select count(distinct E2.Salary)
+    from Employee E2
+    where E2.departmentId = E.departmentId and E2.salary > E.salary 
+) < 3
